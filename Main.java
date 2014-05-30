eb1c35d8878c5ba5bcc4dcec3a0b7c79518571d0
Tarnacop <mrtarnacop@gmail.com>
2014-05-30 18:27:31 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,7 @@
+// Main class used to call the game board constructor
+public class Main {
+	public static void main(String[] args) {
+		
+		new Board();
+	}// END OF main METHOD
+}// END OF Main CLASS
